19d0f6e5083e09f12bb844894c3076f18b80b900
santoshkumar0608 <silarapusantosh@gmail.com>
2021-09-30 12:11:03 +0530

    asf

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 5000
-const url =  "mongodb+srv://deepak:Dipak@1243@cluster0.o7rmv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const url =  process.env.MONGODB_URI || "mongodb+srv://deepak:Dipak@1243@cluster0.o7rmv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true }).then((res)=>{
     console.log("we have a new connection")
 }).catch((err)=>{
